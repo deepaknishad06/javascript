6bef8c1615f8a0a914781584b390fd100bbbeccf
Deepak Nishad <dcreation2706@gmail.com>
2026-04-15 12:06:35 +0530

    update

---
+++ js/revision.py
@@ -0,0 +1,106 @@
+
+"""
+a=(1,2,3,4,5,6)
+print(list(a))
+print(tuple(a))
+print(set(a))
+b={"name":"Alice","age": "old"}
+print(dict(b))
+c={"year":"2000"}
+b.update(c)
+print(b)
+
+a=4
+b=8
+
+c=a+b
+print(c)
+d=a-b
+print(d)
+e=a*b
+print(e)
+f=a**b
+print(f)
+
+h=a//b
+print(h)
+i=a%b
+print(i)
+j=b%a
+print(j)
+k=a|b
+print(k)
+g=a/b
+print(g)
+
+a=7
+b=9
+c=a>=b
+print(c)
+a=7
+b=9
+c=a>=b
+print(c)
+a=int(input("enter a number"))
+if a==3:
+    print("ok")
+else:
+    print("not ok")
+a=int(input("enter a number"))
+if a>10:
+    print("it is a large no")
+    if a<10:
+        print("it is not a large no")
+    else:
+     print("not ok")
+else:
+    print("ok")
+age=int(input("enter a number"))
+if age>=18:
+    print("legal activity")
+elif age<18:
+    print("illegal activity")
+    print("ok")
+
+a="hi"
+b=a.isalnum()
+print(b)
+
+e=a.isdigit()
+print(e)
+f=a.isupper()
+print(f)
+g=a.title()
+print(g)
+
+h="Hello"
+i=h.casefold()
+print(i
+h="Hello"
+i=h.endswith("OK")
+print(i)
+
+h="Hello"
+i=h.startswith("OK")
+print(i)
+
+h=[1,2,3,4,7]
+h.pop(3)
+print(h)
+
+h=[1,2,3,4,7]
+h.remove(3)
+print(h)
+
+h=[1,2,3,4,7]
+print(len(h))
+
+a=[1,2,3,4,56,89]
+print(a*8)
+
+
+a=("hi ")
+b=("hello")
+c=a+b
+print(c)"""
+
